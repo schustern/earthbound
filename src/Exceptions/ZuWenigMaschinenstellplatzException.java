3d0db4f0ecd8fdcfa21beda09b09de708625059b
schustern <niklas.schuster@hpe.com>
2017-02-12 18:54:52 +0100

    Update ZuWenigMaschinenstellplatzException.java

--- src/Exceptions/ZuWenigMaschinenstellplatzException.java
+++ src/Exceptions/ZuWenigMaschinenstellplatzException.java
@@ -1,13 +1,13 @@
 package Exceptions;
 
 /**
- * Exception, die beim Kaufen von Maschinen geworfen wird, falls nicht genügend Maschinenstellplätze (= Produktionshallen) vorhanden sind
+ * Exception, die beim Kaufen von Maschinen geworfen wird, falls nicht gengend Maschinenstellpltze (= Produktionshallen) vorhanden sind
  * Created by lucadommes on 19.01.2017.
  */
 public class ZuWenigMaschinenstellplatzException extends Exception {
 
     public ZuWenigMaschinenstellplatzException() {
-        super("Es sind zu wenige Maschinenstellplätze vorhanden! Kaufen Sie eine weitere Produktionshalle!");
+        super("Es sind zu wenige Maschinenstellpltze vorhanden! Kaufen Sie eine weitere Produktionshalle");
     }
 
 }
